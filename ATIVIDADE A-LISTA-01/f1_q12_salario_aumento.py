e1688be6654ad8c59e801707049ccf80ce0b1b78
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-28 18:46:26 -0300

    Create f1_q12_salario_aumento.py

---
+++ ATIVIDADE A-LISTA-01/f1_q12_salario_aumento.py
@@ -0,0 +1,14 @@
+# Leia o salário de um trabalhador e escreva seu novo salário com um aumento de 25%.
+
+# Entrada
+
+salario_original = float(input('Insira seu salário: '))
+
+# Processamento
+
+salario_novo = salario_original + (salario_original * 0.25)
+
+# Saída 
+
+print(f'Seu salário apos o aumento é ${salario_novo:.2f}')
+
