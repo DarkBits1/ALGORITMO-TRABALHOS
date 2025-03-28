07ebfb468d6ca90edd18cdbf2418d9bc273e8728
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-28 16:16:53 -0300

    Create f1_q7_soma_diferenca.py

---
+++ ATIVIDADE A-LISTA-01/f1_q7_soma_diferenca.py
@@ -0,0 +1,19 @@
+# Leia 3 números, calcule e escreva a soma dos 2 primeiros e a diferença entre os 2 últimos.
+
+# Entrada
+
+numero1 = float(input('Insira o número 1: '))
+numero2 = float(input('Insira o número 2: '))
+numero3 = float(input('Insira o número 3: '))
+
+# Processamento
+
+soma = numero1 + numero2
+
+diferenca = numero2 - numero3
+
+#Saída
+
+print(f'O valor da soma dos dois primeiros é ==> {soma:.2f}\n O valor da diferenca entre os 2 ultimos é {diferenca:.2f}')
+
+
