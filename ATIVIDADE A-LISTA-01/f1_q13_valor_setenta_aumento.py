cbf6c0787088cd4bc4c0eaa04d9c55a12f17f395
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-28 18:49:54 -0300

    Create f1_q13_valor_setenta_aumento.py

---
+++ ATIVIDADE A-LISTA-01/f1_q13_valor_setenta_aumento.py
@@ -0,0 +1,13 @@
+# Leia um valor em real (R$), calcule e escreva 70% deste valor.
+
+# Entrada
+
+valor_original = float(input('Diga o valor em reais: '))
+
+# Processamento 
+
+valor_setenta = valor_original * 0.7
+
+#Saída
+
+print(f'Setenta porcento desse valor é: {valor_setenta:.2f} reais')
