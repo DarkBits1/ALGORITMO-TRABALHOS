0fca05280a3b86d0797959c8ad6004f83acef552
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-28 18:37:03 -0300

    Update f1_q11_numero_inverso_tres.py

--- ATIVIDADE A-LISTA-01/f1_q11_numero_inverso_tres.py
+++ ATIVIDADE A-LISTA-01/f1_q11_numero_inverso_tres.py
@@ -1,1 +1,18 @@
 
+# Leia um número inteiro (3 dígitos) e escreva o inverso do número. (Ex.: número = 532 ; inverso = 235)
+
+# Entrada
+
+numero = int(input('Insira um numero inteiro de 3 digitos: '))
+
+# Processamento
+
+numero_centena = numero // 100
+numero_unidade = numero % 10
+numero_dezena  = (numero % 100) // 10
+
+
+# Saída
+
+
+print(f'{numero_unidade}{numero_dezena}{numero_centena}')
