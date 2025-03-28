33dabc5a4726268ff8f5196898252691d4035e3c
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-28 20:38:58 -0300

    Create f1_q24_metro_cm.py

---
+++ ATIVIDADE A-LISTA-01/f1_q24_metro_cm.py
@@ -0,0 +1,13 @@
+# Leia um valor em m, calcule e escreva o equivalente em cm.
+
+# Entrada
+
+valor_metro = float(input('Insira o valor em metros: '))
+
+# Processamento
+
+valor_centimetro = valor_metro * 100
+
+# Saída
+
+print(f'O valor em centimetros é ==> {valor_centimetro}')
