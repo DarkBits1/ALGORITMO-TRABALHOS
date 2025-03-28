f678ff9d9e04ea3adb18abc918e30fe749c35fc7
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-28 19:01:40 -0300

    Create f1_q15_triangulo_area.py

---
+++ ATIVIDADE A-LISTA-01/f1_q15_triangulo_area.py
@@ -0,0 +1,14 @@
+# Leia o valor da base e altura de um triângulo, calcule e escreva sua área. (área=(base * altura)/2)
+
+# Entrada 
+
+base = float(input('Insira o valor da base: '))
+altura = float(input('Insira o valor da altura: '))
+
+# Processamento 
+
+area = (base * altura) / 2
+
+# Saída
+
+print(f'O valor da área desse triângulo é ==> {area}')
