7dc033b54b7ce0a43011459d72895b8ccb8b303c
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-28 19:13:04 -0300

    Create f1_q17_retangulo_area.py

---
+++ ATIVIDADE A-LISTA-01/f1_q17_retangulo_area.py
@@ -0,0 +1,14 @@
+# Leia o valor da base e altura de um retângulo, calcule e escreva sua área. (área = base * altura)
+
+# Entrada
+
+base = float(input('Insira o valor da base: '))
+altura = float(input('Insira o valor da altura: '))
+
+# Processamento
+
+area = base * altura
+
+# Saída
+
+print(f'O valor da área desse retângulo é ==> {area:.2f}')
