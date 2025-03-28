e15ae48431900b783618cb333578e5eb215811c6
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-28 19:09:20 -0300

    Create f1_q16_quadrado_area.py

---
+++ ATIVIDADE A-LISTA-01/f1_q16_quadrado_area.py
@@ -0,0 +1,13 @@
+# Leia o valor do lado de um quadrado, calcule e escreva sua área. (área = lado2)
+
+# Entrada
+
+lado = float(input('Insira o valor do lado do quadrado: '))
+
+# Processamento
+
+area = lado * lado
+
+# Saída
+
+print(f'O valor da área do quadrado é ==> {area}')
