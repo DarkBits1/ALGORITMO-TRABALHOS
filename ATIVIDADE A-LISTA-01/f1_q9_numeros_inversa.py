b042a6c8123095a474db4abc08fab94a3109cfc5
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-28 16:30:47 -0300

    Create f1_q9_numeros_inversa.py

---
+++ ATIVIDADE A-LISTA-01/f1_q9_numeros_inversa.py
@@ -0,0 +1,14 @@
+# Leia 2 números (A, B) e escreva-os em ordem inversa (B, A).
+
+# Entrada
+
+numeroA = float(input('Insira o numero A: '))
+numeroB = float(input('Insira o numero B: '))
+
+# Processamento
+
+
+
+# Saída
+
+print(f'A ordem inversa dos numero é {numeroB}, {numeroA}')
