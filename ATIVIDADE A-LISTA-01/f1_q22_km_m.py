db4abbb8927969eb11a4cfa5c6be7570dcad290e
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-28 20:32:40 -0300

    Create f1_q22_km_m.py

---
+++ ATIVIDADE A-LISTA-01/f1_q22_km_m.py
@@ -0,0 +1,13 @@
+# Leia um valor em km, calcule e escreva o equivalente em m.
+
+# Entrada
+
+valor_km = float(input('Insira o valor de quilômetros: '))
+
+# Processamento
+
+valor_metros = valor_km * 1000
+
+# Saída
+
+print(f'O valor em metros é ==> {valor_metros:.2f} m')
