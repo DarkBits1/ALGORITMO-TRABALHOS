51ba6943070e06a3eb5f6db468e9a5a7cd1c7fde
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-28 20:36:23 -0300

    Create f1_q23_kg_grama.py

---
+++ ATIVIDADE A-LISTA-01/f1_q23_kg_grama.py
@@ -0,0 +1,13 @@
+# Leia um valor em kg (quilograma), calcule e escreva o equivalente em g (grama).
+
+# Entrada
+
+valor_kg = float(input('Insira o valor em kg (quilograma): '))
+
+# Processamento
+
+valor_grama = valor_kg * 1000
+
+# Saída
+
+print(f'O valor em g (grama) é {valor_grama}')
