77637b5a4ec846e0466676520396e98a08459eb2
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-28 19:18:38 -0300

    Create f1_q18_circunferencia_comprimento.py

---
+++ ATIVIDADE A-LISTA-01/f1_q18_circunferencia_comprimento.py
@@ -0,0 +1,14 @@
+# Leia o valor do raio de uma circunferência, calcule e escreva seu comprimento.(c = 2 * p * r)
+
+# Entrada
+
+raio = float(input('Insira o valor do raio: '))
+
+# Processamento
+
+comprimento = 2 * 3.141592653 * raio
+
+# Saída
+print(f'O valor do comprimento é ==> {comprimento:.2f}')
+
+
