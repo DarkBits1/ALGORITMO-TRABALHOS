044147fa17f963e6f1fdfcada9ad1af8c1068033
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-29 19:26:07 -0300

    Update f1_q39_expressao_calculo.py

--- ATIVIDADE A-LISTA-01/f1_q39_expressao_calculo.py
+++ ATIVIDADE A-LISTA-01/f1_q39_expressao_calculo.py
@@ -1,19 +1,16 @@
- # Leia três números inteiros e positivos (A, B, C) e calcule a seguinte expressão: D = (R + S) / 2 ; R = (A + B)^2 e S = (B + C)^2
+ # Leia três números inteiros e positivos (A, B, C) e calcule a seguinte expressão: D = R + S / 2 ; R = (A + B)^2 e S = (B + C)^2
 
 
 # Entrada
-
 numeroA = int(input('Insira o primeiro valor: '))
 numeroB = int(input('Insira o segundo valor: '))
 numeroC = int(input('Insira o terceiro valor: '))
 
 # Processamento
-R = (numeroA * numeroA) + (numeroB * numeroB)
-S = (numeroB * numeroB) + (numeroC * numeroC)
-expressao = (R + S)/2
+R = (numeroA + numeroB) ** 2 
+S = (numeroB + numeroC) ** 2  
+expressao = (R + S) / 2  
 
 # Saída
-
-print(f'O valor da expressão é ==> {expressao:2f}')
-
+print(f'O valor da expressão é ==> {expressao:.2f}')
 
