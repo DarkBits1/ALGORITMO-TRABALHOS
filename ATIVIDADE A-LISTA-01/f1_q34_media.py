21e30c73831dcc89f8b4d3678f704856cef44796
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-29 15:38:29 -0300

    Create f1_q34_media.py

---
+++ ATIVIDADE A-LISTA-01/f1_q34_media.py
@@ -0,0 +1,15 @@
+# Leia 3 números, calcule e escreva a média dos números.
+
+# Entrada
+
+numero1 = float(input('Insira o primeiro numero: '))
+numero2 = float(input('Insira o segundo numero: '))
+numero3 = float(input('Insira o terceiro numero: '))
+
+# Processamento
+
+media = (numero1 + numero2 + numero3) / 3
+
+# Saída
+
+print(f'A média dos três números é ==> {media:.2f}')
