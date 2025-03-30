bafd03b6f24e4139180ab86e017b65c9e73ca5b3
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-29 21:26:59 -0300

    Update b_q2_soma.py

--- B - Operadores Aritméticos (A)/b_q2_soma.py
+++ B - Operadores Aritméticos (A)/b_q2_soma.py
@@ -1,1 +1,13 @@
-Escreva um programa que calcule a soma dos primeiros N números naturais.
+# Escreva um programa que calcule a soma dos primeiros N números naturais.
+
+# Entrada
+
+numero = int(input('Insira o valor: '))
+
+# Processamento
+
+soma = numero * (numero + 1) // 2
+
+# Saída
+
+print(f'O resultado da soma dos números naturais é ==> {soma}')
