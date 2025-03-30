1d42565493f23c4a2391ccb13d06a7ecd41ba8d9
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-29 21:08:53 -0300

    Create b_q8_IMC.py

---
+++ B - Operadores Aritméticos (A)/b_q8_IMC.py
@@ -0,0 +1,14 @@
+# Escreva um programa que calcule o Índice de Massa Corporal (IMC), dado o peso e a altura.
+
+# Entrada
+
+peso = float(input('Insira o peso: '))
+altura = float(input('Insira a altura: '))
+
+# Processamento
+
+imc = peso / altura
+
+# Saída
+
+print(f'O IMC é ==> {imc}')
