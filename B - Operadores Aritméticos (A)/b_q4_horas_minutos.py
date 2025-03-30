fe8586fa3f4a63f8162948f7420086fe603385ef
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-29 21:34:09 -0300

    Update b_q4_horas_minutos.py

--- B - Operadores Aritméticos (A)/b_q4_horas_minutos.py
+++ B - Operadores Aritméticos (A)/b_q4_horas_minutos.py
@@ -1,1 +1,14 @@
 # Escreva um programa que converta minutos para horas e minutos.
+
+# Entrada
+
+minutos = int(input('Insira a quantidade de minutos: '))
+
+# Processamento
+
+horas = minutos // 60
+minutos_final = minutos % 60
+
+# Saída
+
+print(f'A quantidade traduz para ==> {horas} horas e {minutos_final} minutos')
