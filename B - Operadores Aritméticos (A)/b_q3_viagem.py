06d2df1b029fe50875480ec6e35b18c63ca39578
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-29 21:31:28 -0300

    Update b_q3_viagem.py

--- B - Operadores Aritméticos (A)/b_q3_viagem.py
+++ B - Operadores Aritméticos (A)/b_q3_viagem.py
@@ -1,2 +1,14 @@
-# Escreva um programa que calcule quanto tempo levará uma viagem, dado a distância e a
-velocidade média.
+# Escreva um programa que calcule quanto tempo levará uma viagem, dado a distância e a velocidade média.
+
+# Entrada
+
+velocidade = float(input('Insira a velocidade em km: '))
+distancia = float(input('Insira a distancia em km: '))
+
+# Processamento
+
+tempo = distancia / velocidade
+
+# Saída
+
+print(f'O tempo de duração da viagem é ==> {tempo} horas')
