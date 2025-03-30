d4b8771230622486954d93ff4d57c41459b0b55f
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-29 21:37:57 -0300

    Update b_q5_notas.py

--- B - Operadores Aritméticos (A)/b_q5_notas.py
+++ B - Operadores Aritméticos (A)/b_q5_notas.py
@@ -1,1 +1,14 @@
 # Escreva um programa que determine a quantidade de notas de 50 e 10 necessárias para um determinado valor.
+
+# Entrada
+
+valor = int(input('Insira o valor: '))
+
+# Processamento
+
+nota_50 = valor // 50
+nota_10 = (valor % 50) // 10
+
+# Saída
+
+print(f'Você precisará de {nota_50} nota(s) de R$50 e {nota_10} nota(s) de R$10')
