33cc10decd6d8467326d55746a51ef7aab1960c0
Renato <105134815+DarkBits1@users.noreply.github.com>
2025-03-29 21:42:21 -0300

    Create q1_cdb_cdc.py

---
+++ C - Operadores Aritméticos (B) - CDB/Viagem/q1_cdb_cdc.py
@@ -0,0 +1,23 @@
+Leia, Interprete e faça um simulador para o seguinte problema (Contexto Adaptado).
+Os bancos captam dinheiro para suas operações por meio de CDBs (Certificado de
+Depósito Bancário), dentre outras formas. Por outro lado, eles fazem empréstimos
+aos seus clientes em operações ditas CDC (Crédito Direto ao Consumidor).
+Para o CDB o cliente do Banco receberá uma recompensa pelo investimento com
+taxa de juros sobre valor entre 1% e 20% (ao ano), por exemplo, aplicada em Juros
+Compostos (desconsidere IR - Imposto de Renda).
+Já o CDC é um valor percentual geralmente entre 1% e 17% (ao mês), a depender do
+tipo do empréstimo, em Juros Compostos. O Cliente paga o CDC mensalmente em
+parcelas iguais. Os prazos comuns são 24x, 36x e até 60x.
+O Lucro do Banco é o Juros dos Empréstimos descontado o quanto ele pagará de
+Juros para os Investidores de CDB. Considerando as afirmações acima, faça um
+SIMULADOR de CDB e em conjunto um Simulador de CDC (Empréstimo) do mesmo
+valor captado. Para o CDB, solicite os dados necessários, identifique quais pelo
+contexto.
+Em seguida, calcule quanto o Banco irá pagar (Valor Investido + Juros CDB) para o
+Investidor ao final do período escolhido. Exemplo: Um CDB de vencimento 2035, tem
+um prazo de 10 anos. 2035 - 2025.
+Já no empréstimo, pergunte os demais dados necessários para simular um
+empréstimo e mostre quanto o Cliente CDC irá pagar de Juros ao todo para o Banco.
+Mostre também o valor das Parcelas. E o montante total a pagar.
+Por fim, faça um resumo sobre o Lucro do Banco com as transações de CDB e CDC.
+Incluindo todos os dados das duas operações.
